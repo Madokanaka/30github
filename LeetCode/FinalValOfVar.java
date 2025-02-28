73eb890a5a98848964dc4340acf0e0b318a7aeb3
Seitek <seita20078@gmail.com>
2025-02-28 18:45:58 +0600

    Wrote solution to LeetCode task 2011

---
+++ LeetCode/FinalValOfVar.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int val = 0;
+        for (int i = 0; i < operations.length; i++){
+            if(operations[i].contains("+")) {
+                val++;
+            } else {
+                val--;
+            }
+        }
+        return val;
+    }
+}
